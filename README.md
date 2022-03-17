b954e99d1fdd3bfa35c69a65fac9652dbab31724
Senol Tas <senol.tas@magnolia-cms.com>
2022-03-17 11:11:52 +0100

    added npm package and ci publish step

--- README.md
+++ README.md
@@ -2,16 +2,5 @@
 This is the HA command line tool published to npm. It is a pure wrapper around the ha-cli cli tool published to nexus. 
 If the tool doesn't find ha-cli command it will configure .npmrc promting for nexus credentials.
 
-The cli is developed in Rust. Make sure you install the rust toolchain via https://www.rust-lang.org/tools/install
-
-
-# Build
-```cargo build --release```
-
-
-# NPM 
-The CLI will be distributed via NPM and the npm package is located in the npm directory
-
-
-# Releases
-A github action workflow will automatically build the cli for linux, windows and mac and upload the binary when a new release is created.
+# Documentation
+Please refer to https://ha.magnolia-cms.com/docs/getting-started/hello-ha-cli for more information.
---
+++ npm/binary.js
@@ -0,0 +1,77 @@
+
+const { Binary } = require("binary-install");
+const os = require("os");
+const cTable = require("console.table");
+
+const error = msg => {
+    console.error(msg);
+    process.exit(1);
+};
+
+const { version, name, binaryname, repository } = require("../package.json");
+
+const supportedPlatforms = [
+    {
+        TYPE: "Windows_NT",
+        ARCHITECTURE: "x64",
+        RUST_TARGET: "x86_64-pc-windows-msvc",
+        ARCHIVE_NAME: "x86_64-pc-windows-gnu.tar.gz",
+        BINARY_NAME: "ha-cli-wrapper.exe"
+    },
+    {
+        TYPE: "Linux",
+        ARCHITECTURE: "x64",
+        RUST_TARGET: "x86_64-unknown-linux-musl",
+        ARCHIVE_NAME: "x86_64-unknown-linux-musl.tar.gz",
+        BINARY_NAME: "ha-cli-wrapper"
+    },
+    {
+        TYPE: "Darwin",
+        ARCHITECTURE: "x64",
+        RUST_TARGET: "x86_64-apple-darwin",
+        ARCHIVE_NAME: "x86_64-apple-darwin.tar.gz",
+        BINARY_NAME: "ha-cli-wrapperd"
+    }
+];
+
+const getPlatformMetadata = () => {
+    const type = os.type();
+    const architecture = os.arch();
+
+    for (let index in supportedPlatforms) {
+        let supportedPlatform = supportedPlatforms[index];
+        if (
+            type === supportedPlatform.TYPE &&
+            architecture === supportedPlatform.ARCHITECTURE
+        ) {
+            return supportedPlatform;
+        }
+    }
+
+    error(
+        `Platform with type "${type}" and architecture "${architecture}" is not supported by ${binaryname}.\nYour system must be one of the following:\n\n${cTable.getTable(
+            supportedPlatforms
+        )}`
+    );
+};
+
+const getBinary = () => {
+    const platformMetadata = getPlatformMetadata();
+    const url = `${repository.url}/releases/download/v${version}/${binaryname}_v${version}_${platformMetadata.ARCHIVE_NAME}`;    
+    return new Binary(platformMetadata.BINARY_NAME, url);
+};
+
+const run = () => {
+    const binary = getBinary();
+    binary.run(); 
+};
+
+const install = () => {
+    const binary = getBinary();
+    binary.install();
+};
+
+module.exports = {
+    install,
+    run
+};
